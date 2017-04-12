dc23d4a3c1e689db3b008f643b8281a3246d1e5d
Adam St. John <astjohn@gmail.com>
2017-04-11 21:24:11 -0400

    Adjust railtie to use 'to_prepare'

--- lib/emoji/railtie.rb
+++ lib/emoji/railtie.rb
@@ -4,11 +4,12 @@ require 'sprockets'
 
 module Emoji
   class Railtie < Rails::Railtie
-    initializer "emoji.defaults" do
-      Emoji.asset_host = ActionController::Base.asset_host
+
+    config.to_prepare do
+      Emoji.asset_host ||= ActionController::Base.asset_host
       asset_prefix = Rails.application.config.assets.prefix rescue '/assets'
-      Emoji.asset_path =  File.join(asset_prefix, '/emoji')
-      Emoji.use_plaintext_alt_tags = false
+      Emoji.asset_path ||=  File.join(asset_prefix, '/emoji')
+      Emoji.use_plaintext_alt_tags ||= false
     end
 
     rake_tasks do
